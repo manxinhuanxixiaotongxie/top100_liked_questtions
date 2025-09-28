a9715990ac29d075a6b07b352baef93ed7c76128
yanghm55 <yanghm55@mideqa.com>
2025-09-28 09:48:05 +0800

    翻转链表问题

--- top100/Code024.java
+++ top100/Code024.java
@@ -1,5 +1,9 @@
 package top100;
 
+import org.w3c.dom.ls.LSInput;
+
+import java.util.List;
+
 public class Code024 {
     // 两两交换链表中的节点
     public ListNode swapPairs(ListNode head) {
@@ -18,6 +22,37 @@ public class Code024 {
             pre.next = end;
             pre = begin;
             cur = next;
+        }
+        return newHead.next;
+    }
+
+    /**
+     * 一种通用解法
+     * 设置上一段结束的节点与下一段开始的节点
+     * 翻转在这段链表的中间节点
+     * 后续每K个一组翻转链表都可以这么做
+     * 参考Code025
+     *
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs2(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = new ListNode(-1);
+        newHead.next = head;
+        ListNode lastEnd = newHead;
+        ListNode nextBegin = head.next.next;
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            // 翻转cur与cur.next
+            ListNode next = cur.next;
+            cur.next = nextBegin;
+            next.next = cur;
+            lastEnd.next = next;
+            lastEnd = cur;
+            cur = cur.next;
+            nextBegin = (cur == null || cur.next == null) ? null : cur.next.next;
         }
         return newHead.next;
     }
--- top100/Code025.java
+++ top100/Code025.java
@@ -38,6 +38,51 @@ public class Code025 {
         return pre;
     }
 
+    /**
+     * 通用做法
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        // 影子节点
+        ListNode newHead = new ListNode(-1);
+        newHead.next = head;
+        // 上一组的结束节点
+        ListNode lastEnd = newHead;
+        ListNode cur = head;
+        // 下一组的开始节点
+        ListNode nextBegin;
+        int index = 0;
+        while (cur != null) {
+            nextBegin = cur.next;
+            if (++index == k) {
+                // 翻转从lastEnd到nextBegin之间的节点
+                ListNode lastEndTemp = lastEnd.next;
+                ListNode reverse = reverse2(lastEnd.next, nextBegin);
+                lastEnd.next = reverse;
+                lastEnd = lastEndTemp;
+                index = 0;
+            }
+            cur = nextBegin;
+        }
+        return newHead.next;
+    }
+
+    public ListNode reverse2(ListNode head, ListNode end) {
+        ListNode pre = end;
+        ListNode cur = head;
+        while (cur != end) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+
     public static void main(String[] args) {
         Code025 code025 = new Code025();
         ListNode head = new ListNode(1);
